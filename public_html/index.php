f396e87158d3738ca164012b49ec160fa8ad25e6
Jeff Saul <scaleup13@gmail.com>
2015-10-25 22:53:18 -0600

    Pushing framework for milestone 1

---
+++ public_html/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Saul Web Software Development</title>
+	</head>
+	<body>
+		// This is Phase 1 of the Persoanl Website project for Deep Dive Coding
+		<h1>Saul Web Designs</h1>
+		<<h2>Contact Information</h2>
+		<p></p>
+	</body>
+
+</html>
