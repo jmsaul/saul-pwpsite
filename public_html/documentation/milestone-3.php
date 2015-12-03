4a32c2ec46f0d93a90f6f97392db65936626bdf1
Jeff Saul <scaleup13@gmail.com>
2015-12-03 09:31:44 -0700

    Submission for Milestones 2 and 3

--- public_html/documentation/milestone-3.php
+++ public_html/documentation/milestone-3.php
@@ -8,20 +8,21 @@
 		<!-- This is Milestone 3 of the Persaonal Website project for Deep Dive Coding -->
 		<h1>Jeff - Personal Website Project (PHP) Milestone 3</h1>
 		<h2>Mobile Homepage</h2>
+		<img src="images/mobile-homepage.png" alt="Wireframe for Mobile Homepage">
+		<br>
+
 		<h2>Mobile About Me page</h2>
+		<img src="images/mobile-aboutMe.png" alt="Wireframe for Mobile About Me webpage">
+		<br>
+
 		<h2>Desktop Homepage</h2>
+		<img src="images/desktop-homepage.png" alt="Wireframe for Desktop Homepage">
+		<br>
+
 		<h2>Destop About Me page</h2>
-		<!--The first two lines are the title of the project-->
-		<p>  The purpose of this website is to show potential clients what I can do for them and the range of projects I like to work on.  I was thinking of having 3-4 sample websites:  One for eductional materials, one for artists, one for a small business, and one for non-profits.</p>
-		<hr>
-		<h2>Who is the audience?</h2>
-		<!-- Who is the audience for our website?-->
-		<p>The audience would be potential clients and employers who wish to engage my services.  I am looking at starting my own company to provide web design and development services.  I hope to work with a wide range of clientelle.</p>
-		<hr>
-		<h2>What is the goal?</h2>
-		<!--Main Goal for the persaonal website-->
-		<!--This is a brief description of what the my goal-->
-		<p>The ultiimate goal of the website is to publisize my services and generate interest.  I want potential customers to look at the website and be either pleased enough or at least interested enough to contact me to discuss what I can do for them.  I also want them to see examples of the differnt type of websites I can create.</p>
+		<img src="images/desktop-aboutMe.png" alt="Wireframe for Desktop About Me webpage">
+		<br>
+
 	</body>
 
 </html>
