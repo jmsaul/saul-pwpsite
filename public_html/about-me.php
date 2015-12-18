84f944c4a42035147753c71080bd3503b25d49ed
Jeff Saul <scaleup13@gmail.com>
2015-12-18 07:21:01 -0700

    working on about-me page (take 2)

--- public_html/about-me.php
+++ public_html/about-me.php
@@ -65,57 +65,85 @@
 
 			<main class="main-fomrat">
 				<div class="container">
-					<h2>Hello</h2>
-					<p>This past year I changed careers to become a web designer and web application developer. I am a recent
-						graduate of CNM's Deep Dive Coding Bootcamp, a very intensive, practical course in using PHP, mySQL,
-						CSS, JavaScript, Bootcamp, and Angular to create full web site solutions. You can see some of my
-						websites below. I like the challenge of interesting problems and finding practical solutions. I enjoy
-						working with clients to find web solutions that provide an excellent experience for their users.
+					<h4>PROFESSIONAL STATEMENT: </h4>
+					<p>In my previous career, I focused on the teaching and learning of physics, astronomy, and mathematics at an introductory undergraduate level, most recently at the University of New Mexico.  I earned a Ph.D. in Physics in Physics Education Research.  I am now starting a new career that lets me apply my knowledge and technical skills, a career as a web designer and web application developer.  I enjoy coding and had coded applications both as an undergraduate and graduate student.  I am currently updating my skills to pursue a career in web application design and development.  I like the challenge of interesting problems and finding practical solutions.
 					</p>
-					<a href="about-me.php"><h4>To learn more about me</h4></a>
 				</div>
-				<hr>
 
 				<div class="container">
-					<div class="text-center">
-						<h3>My Portfolio</h3>
-						<div class="row">
-							<div class="col-md-4">
-								<div>
-									<a href="//bootcamp-coders.cnm.edu/~jsaul4/open-trails/public_html/">
-										Trail-Quail Website
-									</a>
-								</div>
-								<a href="//bootcamp-coders.cnm.edu/~jsaul4/open-trails/public_html/">
-									<img src="img/sandiaMtnNM.jpg" alt="Trail Quail Project" class="image-saul img-circle">
-								</a>
-							</div>
-
-							<div class="col-md-4">
-								<div>
-									<a href="//physics.unm.edu/Courses/Saul/00_Classes/FL2014_Saul_PHYC110home.html">
-										Physics 110 Website
-									</a>
-								</div>
-								<a href="//physics.unm.edu/Courses/Saul/00_Classes/FL2014_Saul_PHYC110home.html">
-									<img src="img/shuttle.jpeg" alt="Physics 110 Website" class="image-saul img-circle">
-								</a>
-							</div>
-
-							<div class="col-md-4">
-								<div>
-									<a href="//physics.unm.edu/Courses/Saul/00_Classes/FL2014_A101lab.html">
-										Astro 101lab Website
-									</a>
-								</div>
-								<a href="http://physics.unm.edu/Courses/Saul/00_Classes/FL2014_Saul_PHYC110home.html">
-									<img src="img/crab-nebula.jpeg" alt="Astro 101lab Website" class="image-saul img-circle">
-								</a>
-							</div>
-
-
-						</div>
+					<h4>COMPUTER SKILLS:</h4>
+					<div class="row">
+						<div class="col-xs-3">Languages:</div>
+						<div class="col-xs-9">PHP, CSS, HTML5, MySQL, JavaScript, Bootstrap, Angular, Pascal, Fortran, Forth, Cobol, and APL</div>
 					</div>
+
+					<div class="row">
+						<div class="col-xs-3">Operating Systems:</div>
+						<div class="col-xs-9">Windows (XP & 7),  MAC OS X Operating Systems (10.1.1 through Yosemite), and iOS (through 9.2)</div>
+					</div>
+
+					<div class="row">
+						<div class="col-xs-3">Software:</div>
+						<div class="col-xs-9">Microsoft Office, Google Chrome, Apple Safari, Mozilla Firefox Internet Explorer, Mozilla Firefox, Carbonite, PHPstorm, GitHub, Quicken, Blackboard Learn, Pearson Mastering, Learning Catalytics, iClicker, Socrative, Dreamweaver, Transana, Vernier Logger Pro, Pasco Data Studio, and Google applications</div>
+					</div>
+
+					<div class="row">
+						<div class="col-xs-3">Trouble Shooting:</div>
+						<div class="col-xs-9">Diagnosis of problerms wiht Desktops, laptops, printers, and network connections</div>
+					</div>
+
+				</div>  <!--  End of Computer skills container -->
+
+				<br>
+				<div class="container">
+					<h4>TECHNICAL EXPERIENCE:</h4>
+					<h6>Central New Mexico Community College – Deep Dive Coding Bootcamp </h6>
+					<p>Intensive Practical Web Development Coding Certificate Program (Completed Dec 2015)</p>
+					<ul>
+						<li>
+							<div>
+								<p>Trained as a junior developer with both individual and team projects to code web applications from the backend, the frontend and connecting the two.  Languages: HTML5, CSS, JavaScript, MySQL, Bootstrap, and Angular.</p>
+							</div>
+						</li>
+					</ul>
+
+					<br>
+					<h6>Dept. of Physics and Astronomy, University of New Mexico </h6>
+					<p>(January 2009 - May 2015)</p>
+					<ul>
+						<li>
+							<div>
+								<p>Developed and managed class websites using Dreamweaver for introductory physics and astronomy classes including website for face-to-face and online astronomy lab class.</p>
+							</div>
+						</li>
+
+						<li>
+							<div>
+								<p>Helped design and develop SCALE-UP-style active-learning studio classrooms and the Collaborative Teaching and Learning Building.</p>
+							</div>
+						</li>
+
+						<li>
+							<div>
+								<p>Managed Astronomy Computer Lab.</p>
+							</div>
+						</li>
+
+						<li>
+							<div>
+								<p>Used new technology in the classroom, including video analysis, next generation web-based homework systems, electronic classroom polling, and use of simulations both in class and as homework.  Also, evaluated the use of these systems for my department.</p>
+							</div>
+						</li>
+
+						<li>
+							<div>
+								<p>Reviewed and wrote video scripts for Pearson Education online materials for introductory physics.</p>
+							</div>
+						</li>
+					</ul>
+
+
+
 				</div>
 
 				<br>
